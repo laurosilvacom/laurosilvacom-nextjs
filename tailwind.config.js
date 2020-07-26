b7714240c05b3ec84ae3febadcac1e220492e8b2
Lauro Silva <hey@laurosilva.com>
2020-07-26 16:20:17 -0700

    Adding fovicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const mdx = require('@mdx-js/mdx')
 
 module.exports = {
-  purge: ['./src/**/*.{js,mdx}', './next.config.js'],
+  purge: ['./src/**/*.{js}', './next.config.js'],
   theme: {
     extend: {
       spacing: {
